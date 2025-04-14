e794eee588470c755a8c6a1d9b896d0f12be9cfd
huangdengpan <huangdengpan@hotmail.com>
2025-04-15 00:53:42 +0800

    多了一个逗号

--- ui/shared/grading-standards/jquery/index.js
+++ ui/shared/grading-standards/jquery/index.js
@@ -375,7 +375,7 @@ $(document).ready(() => {
         'assignment[grading_type]': 'letter_grade',
       }
       let url = $('#edit_assignment_form').attr('action')
-      $('input.grading_standard_id, ').val(standard.id)
+      $('input.grading_standard_id').val(standard.id)
       if ($('#update_course_url').length) {
         put_data = {
           'course[grading_standard_id]': standard.id,
